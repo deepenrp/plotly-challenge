bf0fb8143518b92db509db7a660763d415f419fd
Deepen Patel <deepenrp@yahoo.com>
2019-11-24 17:18:01 -0500

    belly-button-diversity

--- belly-button-diversity/static/js/app.js
+++ belly-button-diversity/static/js/app.js
@@ -1,31 +1,85 @@
 
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+  // The following function builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+    var metadata_url = "/metadata/" + sample;
 
-    // Use `.html("") to clear any existing metadata
+    var metadata = d3.select("#sample-metadata");
+    metadata.html("");
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+    d3.json(metadata_url).then(function(sample){
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+      Object.entries(sample).forEach(([key, value]) => {
+        console.log(key,value);
+        metadata.append("h6").text(`${key.toUpperCase()}:- ${value}`);
+      });
+    });
 }
+
 
 function buildCharts(sample) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var sample_url = "/samples/" + sample;
 
-    // @TODO: Build a Bubble Chart using the sample data
+  // Pie Chart using the sample data
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+  d3.json(sample_url).then(function(data) {
+
+    var sample_values = data.sample_values.slice(0,10);
+    console.log(sample_values);
+    var otu_labels = data.otu_labels.slice(0,10);
+    console.log(otu_labels);
+    var otu_ids = data.otu_ids.slice(0,10);
+    console.log(otu_ids);
+
+    var data = [{
+      values: sample_values,
+      labels: otu_ids,
+      hovertext: otu_labels,
+      type: 'pie'
+    }];
+
+    var layout = {
+      height: 400,
+      width: 500
+    };
+
+    Plotly.newPlot('pie', data, layout);
+
+    });
+
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+
+  // Bubble Chart using the sample data
+
+  d3.json(sample_url).then(function(data){
+    var otu_ids = data.otu_ids;
+    var sample_values = data.sample_values;
+    var otu_labels = data.otu_labels;
+    var size = data.sample_values;
+    var colors =  data.otu_ids;
+    var trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {color: colors,
+              opacity: colors,
+                size: size}
+      };
+    var data = [trace1];
+    var layout = {
+      xaxis: {title: "OTU ID"}
+      };
+
+    Plotly.newPlot('bubble', data, layout);
+
+  });
+
 }
+
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -33,7 +87,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
-    console.log(sampleNames);
+    // console.log(sampleNames);
     sampleNames.forEach((sample) => {
       selector
         .append("option", sample)
@@ -48,11 +102,13 @@ function init() {
   });
 }
 
+
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
+
 
 // Initialize the dashboard
 init();
