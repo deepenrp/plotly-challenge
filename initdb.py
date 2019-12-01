bc6780c3923d43be4d8d9704efa7b1d29f90421c
Deepen Patel <deepenrp@yahoo.com>
2019-12-01 17:03:44 -0500

    update app

--- initdb.py
+++
@@ -1,7 +0,0 @@
-from belly_button_diversity.app import belly_button_biodiversity
-from belly_button_diversity.app import belly_button
-
-
-# db.drop_all()
-belly_button_biodiversity.create_all()
-belly_button.create_all()
